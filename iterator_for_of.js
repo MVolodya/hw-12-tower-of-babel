51b562114b9318beddd909008d57a7c11291d068
MVolodya <medvid.volodya@gmail.com>
2017-10-20 01:53:29 +0300

    completed task 8 with iterator for of

---
+++ iterator_for_of.js
@@ -0,0 +1,34 @@
+const max = process.argv[2];
+let FizzBuzz = {
+    [Symbol.iterator]() {
+        let number = 1;
+        return {
+            next() {
+                if (number > max) {
+                    return {done: true};
+                }
+
+                let value = number;
+
+                if (value % 15 === 0) {
+                    value = 'FizzBuzz';
+                } else if (value % 3 === 0) {
+                     value = 'Fizz';
+                } else if (value % 5 === 0) {
+                    value = 'Buzz';
+                }
+
+                number++;
+                
+                return {
+                    done: false,
+                    value: value
+                };
+            }
+        }
+    }
+}
+
+for (let n of FizzBuzz) {
+    console.log(n);
+}
