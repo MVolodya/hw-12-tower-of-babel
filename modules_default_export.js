750218779b25c56813c59e7515cc69315d058870
MVolodya <medvid.volodya@gmail.com>
2017-10-19 19:44:23 +0300

    completed task 5 with default export modules

---
+++ modules_default_export.js
@@ -0,0 +1,33 @@
+/*
+    export const PI = 3.141592;
+
+    var _sqrt = function(s, x, last){
+        return x != last ? _sqrt(s, (x + s / x) / 2.0, x) : x;
+    };
+    export const sqrt = function(s){
+        return _sqrt(s, s/2.0, 0.0);
+    };
+    export const square = function(x){
+        return x * x;
+*/
+
+
+const PI = 3.141592;
+
+let _sqrt = function(s, x, last){
+    return x != last ? _sqrt(s, (x + s / x) / 2.0, x) : x;
+};
+
+function sqrt(s){
+    return _sqrt(s, s/2.0, 0.0);
+};
+
+function square(x) {
+    return x * x;
+};
+
+export default {
+    PI: PI,
+    square: square,
+    sqrt: sqrt
+};
--- modules_with_name.js
+++ modules_with_name.js
@@ -1,23 +1,22 @@
-/*
-    exports.PI = 3.141592;
-
-    var _sqrt = function(s, x, last){
-      return x != last ? _sqrt(s, (x + s / x) / 2.0, x) : x;
-    };
-    exports.sqrt = function(s){
-      return _sqrt(s, s/2.0, 0.0);
-    };
-    exports.square = function(x) {
-      return x * x;
-    };
-*/
 
 
 let arg1 = process.argv[2];
 let arg2 = process.argv[3];
 
-import {PI, sqrt, square} from './modules_default_export_math';
+/*
+    //TASK 4
+    import {PI, sqrt, square} from './modules_default_export_math';
 
-console.log(PI);
-console.log(sqrt(+arg1));
-console.log(square(+arg2));
+    console.log(PI);
+    console.log(sqrt(+arg1));
+    console.log(square(+arg2));
+
+*/
+
+
+    //TASK 5
+    import Math from './modules_default_export';
+
+    console.log(Math.PI);
+    console.log(Math.sqrt(+arg1));
+    console.log(Math.square(+arg2));
